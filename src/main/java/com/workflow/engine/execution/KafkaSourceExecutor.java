af7bdcb5a4fff920cecc0ec2e97739e7bf4150d6
Krishna Sabbu <krishna.sabbu@gmail.com>
2026-02-02 07:46:22 +0530

    changes

--- src/main/java/com/workflow/engine/execution/KafkaSourceExecutor.java
+++ src/main/java/com/workflow/engine/execution/KafkaSourceExecutor.java
@@ -96,7 +96,7 @@ public class KafkaSourceExecutor implements NodeExecutor<Map<String, Object>, Ma
 
             Properties props = new Properties();
             props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-            props.put(ConsumerConfig.GROUP_ID_CONFIG, "bolt-consumer-" + UUID.randomUUID());
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, "batch-consumer-" + UUID.randomUUID());
             props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
             props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
             props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
