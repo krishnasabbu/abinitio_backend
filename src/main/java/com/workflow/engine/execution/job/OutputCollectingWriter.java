1c2d018201aea8023054060499a3b0a778537056
krishnasabbu <krishna.sabbu@gmail.com>
2026-02-07 21:09:10 +0530

    Updated OutputCollectingWriter.java

--- src/main/java/com/workflow/engine/execution/job/OutputCollectingWriter.java
+++ src/main/java/com/workflow/engine/execution/job/OutputCollectingWriter.java
@@ -1,6 +1,9 @@
 package com.workflow.engine.execution.job;
 
 import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStream;
+import org.springframework.batch.item.ItemStreamWriter;
 import org.springframework.batch.item.ItemWriter;
 
 import java.util.ArrayList;
@@ -8,13 +11,34 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-public class OutputCollectingWriter<T> implements ItemWriter<T> {
+public class OutputCollectingWriter<T> implements ItemStreamWriter<T> {
 
     private final ItemWriter<T> delegate;
     private final List<Map<String, Object>> collectedItems = new ArrayList<>();
 
     public OutputCollectingWriter(ItemWriter<T> delegate) {
         this.delegate = delegate;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) {
+        if (delegate instanceof ItemStream stream) {
+            stream.open(executionContext);
+        }
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) {
+        if (delegate instanceof ItemStream stream) {
+            stream.update(executionContext);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (delegate instanceof ItemStream stream) {
+            stream.close();
+        }
     }
 
     @Override
